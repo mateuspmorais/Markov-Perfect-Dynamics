b2a501a7495d5652c57b762518dccec59183c15c
Mateus Morais <54042503+mateuspmorais@users.noreply.github.com>
2020-03-05 16:02:08 -0300

    Update Estoques.R

--- Estoques.R
+++ Estoques.R
@@ -2,6 +2,7 @@
 # Mateus Proença Morais
 #-------------------------------------------------------------------------------------------------------------#
 
+
 rm(list = ls())
 Sys.setlocale("LC_ALL","pt_BR.UTF-8")
 options(encoding = "UTF-8")
@@ -16,17 +17,15 @@ library(tseries)
 library(ggfortify)
 library(cowplot)
 library(stargazer)
-library(extrafont)
-library(extrafontdb)
-library(showtext)
 library(AER)
 library(extraDistr)
 library(e1071)
 library(stats)
 
+path <- "C:\\Users\\mateus.morais\\Downloads\\Estoques.txt"
 #reading and cleaning data
 set.seed(123)
-Estoques <- read_table2("http://www.economia.puc-rio.br/lrezende/OI1/Estoques.txt", 
+Estoques <- read_table2(path, 
                         col_names = c('loja','t','i','x'))
 Estoques <- Estoques[-nrow(Estoques),]
 Estoques <- pdata.frame(Estoques, index = c('loja','t'))
@@ -44,8 +43,8 @@ Estoques$lag_x[is.na(Estoques$lag_x)] <- 0
 Estoques$n <- Estoques$i + Estoques$x - Estoques$forw_i
 Estoques$m <- 0
 for(i in 1:nrow(Estoques)) {
-    row <- Estoques[i,]
-   if(row$t == 10){Estoques[i,9] <- NA}else{Estoques[i,9] <- row$n}
+  row <- Estoques[i,]
+  if(row$t == 10){Estoques[i,9] <- NA}else{Estoques[i,9] <- row$n}
 }
 Estoques[1889,9] =6
 #investigating the statistical properties of n
@@ -63,7 +62,7 @@ ggplot(data = Estoques, aes(x = m, y = i)) + stat_summary(fun.data=mean_cl_norma
   labs(y = 'Estoque', x = 'Unidades de Produto Vendidas') + 
   ggtitle('Estoque X Vendas') + theme_classic() +
   theme(plot.title = element_text(lineheight=2, face="bold", size = 14, hjust = 0.5))
-  
+
 
 #estimation of the distribution of n
 
@@ -83,34 +82,34 @@ politica <- data.frame(prob0 = numeric(), prob12 = numeric(),
                        prob13 = numeric(), prob14 = numeric(), prob15 = numeric(),
                        prob16 = numeric(), prob17 = numeric(), prob18 = numeric())
 for(j in 0:max(Estoques$i)) {
-politica[j + 1,1] <- mean(Estoques$x[Estoques$i==j] == 0)
+  politica[j + 1,1] <- mean(Estoques$x[Estoques$i==j] == 0)
 }
 for(j in 0:max(Estoques$i)) {
-politica[j + 1,2] <- mean(Estoques$x[Estoques$i==j] == 12)
+  politica[j + 1,2] <- mean(Estoques$x[Estoques$i==j] == 12)
 }
 for(j in 0:max(Estoques$i)) {
-politica[j + 1,3] <- mean(Estoques$x[Estoques$i==j] == 13)
+  politica[j + 1,3] <- mean(Estoques$x[Estoques$i==j] == 13)
 }
 for(j in 0:max(Estoques$i)) {
-politica[j + 1,4] <- mean(Estoques$x[Estoques$i==j] == 14)
+  politica[j + 1,4] <- mean(Estoques$x[Estoques$i==j] == 14)
 }
 for(j in 0:max(Estoques$i)) {
-politica[j + 1,5] <- mean(Estoques$x[Estoques$i==j] == 15)
+  politica[j + 1,5] <- mean(Estoques$x[Estoques$i==j] == 15)
 }
 for(j in 0:max(Estoques$i)) {
-politica[j + 1,6] <- mean(Estoques$x[Estoques$i==j] == 16)
-}
-
-for(j in 0:max(Estoques$i)) {
-politica[j + 1,7] <- mean(Estoques$x[Estoques$i==j] == 17)
+  politica[j + 1,6] <- mean(Estoques$x[Estoques$i==j] == 16)
 }
 
 for(j in 0:max(Estoques$i)) {
-politica[j + 1,8] <- mean(Estoques$x[Estoques$i==j] == 18)
+  politica[j + 1,7] <- mean(Estoques$x[Estoques$i==j] == 17)
+}
+
+for(j in 0:max(Estoques$i)) {
+  politica[j + 1,8] <- mean(Estoques$x[Estoques$i==j] == 18)
 }
 
 for(j in 1:7){
-politica[j + 21,] <- c(1,0,0,0,0,0,0,0)
+  politica[j + 21,] <- c(1,0,0,0,0,0,0,0)
 }
 ggplot(data = Estoques, aes(x = i, y = x)) + stat_summary(fun.data=mean_cl_normal) + 
   geom_smooth(method='lm',formula= Estoques$i ~ Estoques$x) +
@@ -151,7 +150,7 @@ action_hat <- array(dim = c(3000,500))
 it <- i0
 it_hat <- array(dim = c(3000,500))
 for(i in 1:500){
-it_hat[,i] = i0
+  it_hat[,i] = i0
 }
 politica_hat <- array(dim = c(500))
 politica_hat <- rnorm(500,8,1)
@@ -159,60 +158,61 @@ politica_hat <- rnorm(500,8,1)
 
 #Loop
 for(i in 1:100) {
-shocks <- rdunif(3000, 1, 8)
-
-for(j in 1:length(it)) {
-action[j,1] <- rdiscrete(1, probs = unlist(politica[it[j]+ 1,]), values = c(0,12,13,14,15,16,17,18))
-}  
-
-#profit
-w[,1] <- w[,1] + ((0.95)^(i - 1))*10*pmin(it + unlist(action), unlist(shocks))
-w[,2] <- w[,2] + ((0.95)^(i - 1))*unlist(action) 
-w[,3] <- w[,3] + ((0.95)^(i - 1))*apply(action,2,function(action)ifelse((action>0),1,0))
-w[,4] <- w[,4] + ((0.95)^(i - 1))*unlist(it)^2  
-
-#transition
-it = pmax.int(it + unlist(action) - shocks,0)
-
-
-#profit of alternative policies
-
-for(j in 1:500){
-for(l in 1:3000){
-if(it_hat[l,j] < politica_hat[j]){action_hat[l,j] <- rdiscrete(1,probs = unlist(politica[it_hat[l,j]+ 1,]),
-                                                         values = c(0,12,13,14,15,16,17,18))} else {
-                                                           action_hat[l,j] <- 0
-                                                         }}
-action_hat[,j] <- ifelse(action_hat[,j] > 0 , max(action_hat[,j] + round(rnorm(1,0,0.1)),0), 0)
+  shocks <- rdunif(3000, 1, 8)
   
-subset <- action_hat[,j]
-dummy <- apply(array(subset),1,function(subset)ifelse((subset>0),1,0))
+  for(j in 1:length(it)) {
+    action[j,1] <- rdiscrete(1, probs = unlist(politica[it[j]+ 1,]), values = c(0,12,13,14,15,16,17,18))
+  }  
+  
   #profit
-  w_hat[,1,j] <- w_hat[,1,j] + ((0.95)^(i - 1))*10*pmin(it_hat[j,] + unlist(action_hat[,j]), unlist(shocks))
-  w_hat[,2,j] <- w_hat[,2,j] + ((0.95)^(i - 1))*unlist(action_hat[,j]) 
-  w_hat[,3,j] <- w_hat[,3,j] + ((0.95)^(i - 1))*dummy
-  w_hat[,4,j] <- w_hat[,4,j] + ((0.95)^(i - 1))*unlist(it_hat[j,])^2  
+  w[,1] <- w[,1] + ((0.95)^(i - 1))*10*pmin(it + unlist(action), unlist(shocks))
+  w[,2] <- w[,2] + ((0.95)^(i - 1))*unlist(action) 
+  w[,3] <- w[,3] + ((0.95)^(i - 1))*apply(action,2,function(action)ifelse((action>0),1,0))
+  w[,4] <- w[,4] + ((0.95)^(i - 1))*unlist(it)^2  
   
   #transition
-  it_hat[,j] = pmax.int(it_hat[,j] + unlist(action_hat[,j]) - shocks,0)
-}}
+  it = pmax.int(it + unlist(action) - shocks,0)
+  
+  
+  #profit of alternative policies
+  
+  for(j in 1:500){
+    for(l in 1:3000){
+      if(it_hat[l,j] < politica_hat[j]){action_hat[l,j] <- rdiscrete(1,probs = unlist(politica[it_hat[l,j]+ 1,]),
+                                                                     values = c(0,12,13,14,15,16,17,18))} else {
+                                                                       action_hat[l,j] <- 0
+                                                                     }}
+    action_hat[,j] <- ifelse(action_hat[,j] > 0 , max(action_hat[,j] + round(rnorm(1,0,0.1)),0), 0)
+    
+    subset <- action_hat[,j]
+    dummy <- apply(array(subset),1,function(subset)ifelse((subset>0),1,0))
+    #profit
+    w_hat[,1,j] <- w_hat[,1,j] + ((0.95)^(i - 1))*10*pmin(it_hat[j,] + unlist(action_hat[,j]), unlist(shocks))
+    w_hat[,2,j] <- w_hat[,2,j] + ((0.95)^(i - 1))*unlist(action_hat[,j]) 
+    w_hat[,3,j] <- w_hat[,3,j] + ((0.95)^(i - 1))*dummy
+    w_hat[,4,j] <- w_hat[,4,j] + ((0.95)^(i - 1))*unlist(it_hat[j,])^2  
+    
+    #transition
+    it_hat[,j] = pmax.int(it_hat[,j] + unlist(action_hat[,j]) - shocks,0)
+  }}
 end_time_w <- Sys.time()
 
 wh_mean<- data.frame(matrix(ncol = 4, nrow = 500))
 for(i in 1:500){
-wh_mean[i,1] <- mean(w_hat[,1,i])
-wh_mean[i,2] <- - mean(w_hat[,2,i])
-wh_mean[i,3] <- - mean(w_hat[,3,i])
-wh_mean[i,4] <- - mean(w_hat[,4,i])
+  wh_mean[i,1] <- mean(w_hat[,1,i])
+  wh_mean[i,2] <- - mean(w_hat[,2,i])
+  wh_mean[i,3] <- - mean(w_hat[,3,i])
+  wh_mean[i,4] <- - mean(w_hat[,4,i])
 }
 
+w_mean <- w[1,]
 w_mean[1,1] <- mean(w[,1])
 w_mean[1,2] <- - mean(w[,2])
 w_mean[1,3] <- - mean(w[,3])
 w_mean[1,4] <- - mean(w[,4])
 g <- matrix(ncol = 4, nrow = 500)
 for(k in 1:500){
-g[k,] <- as.matrix(w_mean - wh_mean[k,])
+  g[k,] <- as.matrix(w_mean - wh_mean[k,])
 }
 
 
@@ -224,5 +224,5 @@ minfunc <- function(par) {sum(pmin(g[,1] + g[,2]*par[1] + g[,3]*par[2] + g[,4]*p
 
 end_time_min <- Sys.time()
 
-
+end_time_w - start_time_w
 
